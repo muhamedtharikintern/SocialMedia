fc5780e192623e04f9b12205e8282b48d7a23325
muhamedtharikintern <muhammadtharikintern@gmail.com>
2026-05-23 10:55:47 +0530

    modified index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
+dotenv.config();
 import app from "./src/app.js";
 import { connectToDatabase } from "./db.js";
 
-dotenv.config();
 
 const port = parseInt(process.env.PORT || "4000", 10);
 
